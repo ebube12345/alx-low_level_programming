84b82cebf6db8c6cc39f0e0c47e3135e84a75742
ebube12345 <opataebubechukwu@gmail.com>
2022-07-01 01:23:36 +0000

    l

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main -prints the phrase with the proper grammar
+ * Return :0 if executed properly ,non-zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
