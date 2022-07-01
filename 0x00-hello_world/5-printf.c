03e8e4e340292ed9d04389b7e96ac85ffb6df66b
ebube12345 <opataebubechukwu@gmail.com>
2022-07-01 03:03:00 +0000

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase with proper grammar, but the outcome is a piece of art,
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
  * Return: it bascically returns o if your code is correct 
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
