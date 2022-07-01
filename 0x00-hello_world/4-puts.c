cd9d0c62dda4f11067f587ca7554ab62c1c00b42
ebube12345 <opataebubechukwu@gmail.com>
2022-07-01 02:14:15 +0000

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main -prints the phrase with the proper grammar
- * Return :0 if executed properly ,non-zero
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
