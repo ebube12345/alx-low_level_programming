c8d29769cadeebf50274b37adcce3da9ba9ee56e
ebube12345 <opataebubechukwu@gmail.com>
2022-07-01 02:22:04 +0000

    l

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this my main it basically checks if your grammar is correct
+ * Return: it bascically returns o if your code is correct 
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
